125c5baf691e3e7dbc6f9da6363e6523666a4a3c
LAPTOP-HCIPOEOR\Kaaw_ <chonthee.psec@gmail.com>
2022-08-29 23:42:18 +0700

    complete lesson project

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const path = require('path')
 const router=require('./routes/myRouter')
 const mongoose = require('mongoose')
 const app = express()
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 app.set('views',path.join(__dirname,'views')) // file location
 app.set('view engine','ejs') // setting template engine
+
+app.use(cookieParser())
+app.use(session({secret:"mysession", resave:false,saveUninitialized:false}))
 app.use(express.urlencoded({extende:false}))
-
 app.use(router)
-
-//goto index html only
 app.use(express.static(path.join(__dirname,'public'))) // Can change
 
 app.listen(8080,()=>{
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -27,24 +27,96 @@ const upload = multer({
 //Home page
 router.get('/',(req,res)=>{
     Product.find().exec((err,doc)=>{
+        //console.log("this is product",doc)
         res.render('index.ejs',{products:doc}) // to render view / template
     })
 })
 
 //Form Add
 router.get('/addForm',(req,res)=>{
-    const products = [
-        {name: "Computer",price:50000,image:"images/products/product1.png"},
-        {name: "Shirt",price:200,image:"images/products/product2.png"},
-        {name: "Headphone",price:1700,image:"images/products/product3.png"}
-    ]
-    res.render('form',{products:products}) // to render view / template
+    // if(req.cookies.login){
+    //     res.render('form')
+    // }else{
+    //     res.render('admin')
+    // }
+    // res.render('admin') // to render view / template
+
+    if(req.session.login){
+        res.render('form')
+    }
+    else{
+        res.render('admin')
+    }
 })
 
 //Form Manage
 router.get('/manage',(req,res)=>{
-    Product.find().exec((err,doc)=>{
-        res.render('manage.ejs',{products:doc}) // to render view / template
+    // if(req.cookies.login){
+    //     Product.find().exec((err,doc)=>{
+    //         res.render('manage.ejs',{products:doc}) // to render view / template
+    //     })
+    // }
+    // else{
+    //     res.render('admin')
+    // }
+
+    //Show data in session
+    console.log('Session >> ', req.sessionID)
+    console.log('Data Session >> ', req.session)
+    if(req.session.login)
+    {
+        Product.find().exec((err,doc)=>{
+            res.render('manage',{products:doc})
+        })
+    }
+    else{
+        res.render('admin')
+    }
+})
+
+//Login
+router.post('/login',(req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+    const timeExpire = 20000 //20 s
+
+    // Setting Cookie
+    // console.log('username >>', username)
+    // console.log('password >>', password)
+    
+    // if(username ==="admin" && password ==="123"){
+    //     res.cookie('username',username,{maxAge:timeExpire})
+    //     res.cookie('password',password,{maxAge:timeExpire})
+    //     res.cookie('login',true,{maxAge:timeExpire}) // true => login เข้าสู่ระบบ
+    //     res.redirect('/manage')
+    // }
+    // else{
+    //     res.render('404')
+    // }
+
+        
+    if(username ==="admin" && password ==="123"){
+        req.session.username = username
+        req.session.password = password
+        req.session.login = true
+        req.session.cookie.maxAge = timeExpire
+        res.redirect('/manage')
+
+    }
+    else{
+        res.render('404')
+    }
+})
+
+//Logout
+router.get('/logout',(req,res)=>{
+    // res.clearCookie('username')
+    // res.clearCookie('password')
+    // res.clearCookie('login')
+    // res.redirect('/manage')
+
+    req.session.destroy((err)=>{
+        res.redirect('/manage')
     })
 })
 
@@ -62,5 +134,51 @@ router.post('/insert',upload.single("image"),(req,res)=>{
         res.redirect('/')
     })
 })
+
+//Delete product
+router.get('/delete/:id',(req,res)=>{
+    console.log("Delete id >>" ,req.params.id)
+    Product.findByIdAndDelete(req.params.id,{useFindAndModify:false}).exec(err=>{
+        if(err) console.log(err)
+        res.redirect('/manage')
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    const product_id = req.params.id
+    //console.log(product_id)
+    Product.findOne({_id:product_id}).exec((err,doc)=>{
+        res.render('product',{product:doc})
+    })
+})
+
+//Edit
+router.post('/Edit',(req,res)=>{
+    const edit_id = req.body.edit_id
+    console.log(edit_id)
+    Product.findOne({_id:edit_id}).exec((err,doc)=>{
+        //Bring that want to edit to show in form
+        res.render('edit',{product:doc})
+    })
+})
+
+//update
+router.post('/update',(req,res)=>{
+    const update_id = req.body.update_id
+    let data = {
+        name:req.body.name,
+        price:req.body.price,
+        description:req.body.description
+    }
+     Product.findByIdAndUpdate(update_id,data,{useFindAndModify:false}).exec(err=>{
+         res.redirect('/manage')
+     })
+    console.log("update id >> ", update_id)
+    console.log("New data", data)
+
+})
+
+//
+
 
 module.exports=router
